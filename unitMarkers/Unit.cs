670e66be95b8ffed3fffc5bcae65bf16ac7b1568
CountBelisarius <andy@belisarius.org.uk>
2016-11-17 14:21:28 +0000

    Added unit

--- unitMarkers/Unit.cs
+++ unitMarkers/Unit.cs
@@ -3,8 +3,46 @@ namespace belisarius.unitMarkers
 {
 	public class Unit
 	{
+		private string _name
+		{
+			get
+			{
+				return _name;
+			}
+
+			set
+			{
+				if (value.Length == 0 || value.Length > 50)
+				{
+					throw new OverflowException();
+				}
+				_name = value;
+
+			}
+		}
+
+		private int _strength
+		{
+			get
+			{
+				return _strength;
+			}
+
+			set
+			{
+				if (value < 1 || value > 5000)
+				{
+					throw new OverflowException();
+				}
+				_strength = value;
+			}
+
+		}
+
+
 		public Unit()
 		{
+			
 		}
 	}
 }
